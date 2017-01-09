fc04dd3ca29e179c4c8c8dd4bf15607ab2de7b8d
PythonOS <root@localhost.localdomain>
2017-01-09 09:57:00 +0800

    change head title

--- mis/__init__.py
+++ mis/__init__.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
-admin.site.site_header = 'Django-ERP'
-admin.site.site_title = 'ERP'
+#admin.site.site_header = 'Django-ERP'
+#admin.site.site_title = 'ERP'
+
+admin.site.site_header = 'Jinghu-CRM'
+admin.site.site_title = 'CRM'
